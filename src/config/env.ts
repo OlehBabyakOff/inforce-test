9544c912b0becf76b7a43fe43bca651587a541c7
Oleh Babyak <babyakwork@gmail.com>
2026-04-23 17:48:44 +0300

    added config file + joi schema

---
+++ src/config/env.ts
@@ -0,0 +1,57 @@
+import 'dotenv/config';
+
+import { validator } from '../infrastructure/validator/joi.validator.js';
+
+import { envSchema } from '../schemas/env.schema.js';
+
+interface Env {
+  NODE_ENV: 'development' | 'production';
+  PORT: number;
+
+  REDIS: {
+    HOST: string;
+    PORT: number;
+    USER: string;
+    PASSWORD: string;
+    KEY_PREFIX: string;
+    MAX_RETRIES_PER_REQUEST: number;
+    ENABLE_OFFLINE_QUEUE: boolean;
+    CONNECT_TIMEOUT: number;
+    COMMAND_TIMEOUT: number;
+  };
+}
+
+export const ENV = (() => {
+  const envToValidate = {
+    NODE_ENV: process.env.NODE_ENV,
+    PORT: process.env.PORT,
+    CORS_ORIGIN: process.env.CORS_ORIGIN,
+    REDIS: {
+      HOST: process.env.REDIS_HOST,
+      PORT: process.env.REDIS_PORT,
+      USER: process.env.REDIS_USER,
+      PASSWORD: process.env.REDIS_PASSWORD,
+      KEY_PREFIX: process.env.REDIS_KEY_PREFIX,
+      MAX_RETRIES_PER_REQUEST: process.env.REDIS_MAX_RETRIES_PER_REQUEST,
+      ENABLE_OFFLINE_QUEUE: process.env.REDIS_ENABLE_OFFLINE_QUEUE,
+      CONNECT_TIMEOUT: process.env.REDIS_CONNECT_TIMEOUT,
+      COMMAND_TIMEOUT: process.env.REDIS_COMMAND_TIMEOUT,
+    },
+  };
+
+  try {
+    const validatedEnv: Env = validator.validateEnv(envSchema, envToValidate);
+
+    return {
+      NODE_ENV: validatedEnv.NODE_ENV,
+      PORT: validatedEnv.PORT,
+
+      REDIS: validatedEnv.REDIS,
+    };
+  } catch (error: any) {
+    console.error(error);
+
+    process.exit(1);
+  }
+})();
+
