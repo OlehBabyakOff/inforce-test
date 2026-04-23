70b7d7f0323ce6f9111c24229dca4beded613ff6
Oleh Babyak <babyakwork@gmail.com>
2026-04-23 23:45:03 +0300

    added user router

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,4 +1,15 @@
 import { Router } from 'express';
 
+import { userController } from '../controllers/user.controller.js';
+
 export const usersRouter = Router();
+
+usersRouter.get('/', userController.findUsers);
+usersRouter.get('/:id', userController.findUser);
+
+usersRouter.post('/', userController.createUser);
+
+usersRouter.put('/:id', userController.updateUser);
+
+usersRouter.delete('/:id', userController.deleteUser);
 
