08c029943aefc0a280af3bc77f3458f1c33c696d
Oleh Babyak <babyakwork@gmail.com>
2026-04-23 23:45:09 +0300

    added book router

--- src/routes/books.router.ts
+++ src/routes/books.router.ts
@@ -1,4 +1,15 @@
 import { Router } from 'express';
 
+import { bookController } from '../controllers/book.controller.js';
+
 export const booksRouter = Router();
+
+booksRouter.get('/', bookController.findBooks);
+booksRouter.get('/:id', bookController.findBook);
+
+booksRouter.post('/', bookController.createBook);
+
+booksRouter.put('/:id', bookController.updateBook);
+
+booksRouter.delete('/:id', bookController.deleteBook);
 
