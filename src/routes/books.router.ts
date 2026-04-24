fb6122f1532a193777901d45deb459347e755a2c
Oleh Babyak <babyakwork@gmail.com>
2026-04-24 17:44:32 +0300

    updated routes with middlewares

--- src/routes/books.router.ts
+++ src/routes/books.router.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 
+import { requireRoleMiddleware } from '../middlewares/role.middleware.js';
+
 import { bookController } from '../controllers/book.controller.js';
 
 export const booksRouter = Router();
 
 booksRouter.get('/', bookController.findBooks);
 booksRouter.get('/:id', bookController.findBook);
+
+booksRouter.use(requireRoleMiddleware(['admin']));
 
 booksRouter.post('/', bookController.createBook);
 
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
+import { requireRoleMiddleware } from '../middlewares/role.middleware.js';
+
 import { userController } from '../controllers/user.controller.js';
 
 export const usersRouter = Router();
+
+usersRouter.use(requireRoleMiddleware(['admin']));
 
 usersRouter.get('/', userController.findUsers);
 usersRouter.get('/:id', userController.findUser);
